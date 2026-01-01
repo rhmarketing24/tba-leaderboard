b8f7099bee75b8da7b3718d2a66cfd62a0609321
RAKTIAR HOSSAIN <rhmarketing24@gmail.com>
2026-01-01 14:48:15 +0600

    cleanup: remove indexer & unused backend

--- indexer/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// indexer/config.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DISTRIBUTOR_ADDRESS = exports.USDC_ADDRESS = exports.BASE_RPC = void 0;
-exports.BASE_RPC = "https://mainnet.base.org";
-exports.USDC_ADDRESS = "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913";
-exports.DISTRIBUTOR_ADDRESS = "0x3d483c284ba397c1ab05e7f74593a79952a812ac";
